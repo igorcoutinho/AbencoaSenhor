382cf1098e77d667ef628f0482f932e5de9df908
Igor Alencar de lisboa Coutinho <igoralcoutinho@gmail.com>
2019-04-14 04:11:20 -0300

    iniciacao do projeto #abencoaSenhor

---
+++ src/main/java/com/checklist/api/AgoraVaiApplication.java
@@ -0,0 +1,13 @@
+package com.checklist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraVaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraVaiApplication.class, args);
+	}
+
+}
